f8e3cf45da34671b73da81214e5ddd9bccf77ac9
Ted <tederkkila@gmail.com>
2026-06-23 21:20:27 +1200

    Update sharp for linux

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,11 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    experimental: {
-        serverExternalPackages: ['sharp'],
-    },
+    serverExternalPackages: ['sharp'],
 };
 
-export default withPayload(nextConfig);
+//export default withPayload(nextConfig);
+export default withPayload(nextConfig, {
+    // Try setting this to false if the error persists
+    devBundleServerPackages: false
+})
