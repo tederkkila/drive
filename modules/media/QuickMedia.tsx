0b1d8b0974e688574dc1a7564d81da9046c209dc
Ted <tederkkila@gmail.com>
2026-06-24 12:40:31 +1200

    Fix errors in QuickMedia component

--- modules/media/QuickMedia.tsx
+++ modules/media/QuickMedia.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import { getPayload } from 'payload';
 import config from '@payload-config';
-import { MediaType } from './types'; // path to your types
+import { Media } from "@/payload-types";
 
 interface ImageProps {
     imageId: string;
@@ -16,11 +16,18 @@ export default async function PayloadImage({ imageId, width, height }: ImageProp
     const media = (await payload.findByID({
         collection: 'media',
         id: imageId,
-    })) as MediaType;
+    })) as Media;
 
     // Fallback if no media or URL is found
     if (!media || !media.url) {
         return <div className="image-placeholder">No image found</div>;
+    }
+
+    const imageWidth = width ?? media.width;
+    const imageHeight = height ?? media.height;
+
+    if (!imageWidth || !imageHeight) {
+        return <div className="image-placeholder">Image dimensions missing</div>;
     }
 
     return (
@@ -28,10 +35,9 @@ export default async function PayloadImage({ imageId, width, height }: ImageProp
             <Image
                 src={media.url}
                 alt={media.alt || 'Uploaded asset'}
-                width={width ||media.width}
-                height={media.height}
+                width={imageWidth}
+                height={imageHeight}
                 priority={true} // Add if this image appears above the fold
-                className="object-cover rounded-lg"
             />
         </div>
     );
