fdf694e608ee0d08f3b71672e279d592725da64d
Ted <tederkkila@gmail.com>
2026-06-27 19:46:44 +1200

    Change api path to api/drive and force relative links in client.tsx

--- trpc/client.tsx
+++ trpc/client.tsx
@@ -30,9 +30,10 @@ function getQueryClient() {
 function getUrl() {
     const base = (() => {
         if (typeof window !== 'undefined') return '';
-        return process.env.NEXT_PUBLIC_APP_URL;
+        /*return process.env.NEXT_PUBLIC_APP_URL;*/
+        return 'http://localhost:3000';
     })();
-    return `${base}/api/trpc`;
+    return `${base}/api/drive`;
 }
 
 export function TRPCReactProvider(
