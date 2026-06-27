df435379516bc505dd4f42e724e9c105bfc7f407
Ted <tederkkila@gmail.com>
2026-06-27 23:54:12 +1200

    More fixes for trpc

--- app/(app)/(tenants)/tenants/[tenantSlug]/layout.tsx
+++ app/(app)/(tenants)/tenants/[tenantSlug]/layout.tsx
@@ -2,27 +2,32 @@ import React from 'react';
 import { headers } from 'next/headers';
 import { TRPCReactProvider } from '@/trpc/client';
 
+interface TenantLayoutProps {
+    children: React.ReactNode;
+    params: Promise<{ tenantSlug: string }>;
+}
+
 export default async function TenantLayout({
    children,
    params,
-}: {
-    children: React.ReactNode;
-    params: Promise<{ tenantSlug: string }>;
-}) {
+}: TenantLayoutProps) {
     const { tenantSlug } = await params;
 
     const headersList = await headers();
-    const host = headersList.get('x-forwarded-host') ?? headersList.get('host');
-    const proto = headersList.get('x-forwarded-proto') ?? 'https';
+
+    const host =
+        headersList.get('x-forwarded-host') ??
+        headersList.get('host');
+
+    const proto =
+        headersList.get('x-forwarded-proto') ??
+        'https';
 
     if (!host) {
         throw new Error('Missing request host');
     }
 
     const origin = `${proto}://${host}`;
-
-    console.log('origin', origin);
-    console.log('tenantSlug', tenantSlug);
 
     return (
         <TRPCReactProvider origin={origin} tenantSlug={tenantSlug}>
